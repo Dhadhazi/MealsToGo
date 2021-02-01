8e46b9231203dcf703a5a8d40ab68c5565f55d6e
Dhadhazi <dhadhazi@gmail.com>
2021-02-01 16:45:31 +0100

    Andorid safeview fix

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.searchBar}>
-        <Text>Search</Text>
-      </View>
-      <View style={styles.listContainer}>
-        <Text>list</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.searchBar}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
+    marginTop: StatusBar.currentHeight,
   },
   searchBar: {
     padding: 16,
